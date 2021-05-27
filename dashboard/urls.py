9373fc58e8dbf82b4666c1df8aaf4f58c065dd80
George Oduor <george.wamaya@gmail.com>
2021-05-27 17:15:41 +0300

    new

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import fb_dashboard,lk_dashboard,tw_dashboard,nba
 app_name = 'dashboard'
 urlpatterns = [
-    path('facebook/', fb_dashboard, name="facebook"),
+    path('', fb_dashboard, name="facebook"),
     path('linkedin/', lk_dashboard, name="linkedin"),
     path('twitter/', tw_dashboard, name="twitter"),
     path('nba/', nba, name="nba")
